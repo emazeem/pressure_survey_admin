68b08e55ce5d1ab515d70f6f8dd27016b9f22b0c
Napollo-Software <emazeem07@gmail.com>
2023-04-21 03:20:22 +0500

    fd

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use \Illuminate\Support\Facades\Auth;
-
+use \Illuminate\Support\Facades\Artisan;
 Auth::routes();
 Route::get('/',function (){
     return redirect()->to('login');
 });
+Route::get('/commands',function (){
+    Artisan::call('optimize');
+    dd(1);
+});
+
 Route::group([],__DIR__.'/partials/super_admin.php');
